1c44c75af8b603818eeb244f808d6edeb827652a
lehoolife <lehoo.life@gmail.com>
2014-06-25 10:10:57 +0800

    更新媒体播放器通用类

--- src/com/psd/app01/util/MediaUtil.java
+++ src/com/psd/app01/util/MediaUtil.java
@@ -8,41 +8,54 @@ import android.media.MediaPlayer;
  */
 public class MediaUtil {
 	private static MediaPlayer player = null;
+
 	/**
 	 * ²¥·Å
+	 * 
 	 * @param path
 	 */
 	public static void play(String path) {
-		path = "http://127.0.0.1:8080/1.mp3";
-		if(player == null) player = new MediaPlayer();
+		// path = "http://127.0.0.1:8080/1.mp3";
 		try {
-			player.setDataSource(path);
-			player.prepare();
+			if (player == null) {
+				player = new MediaPlayer();
+				player.setDataSource(path);
+				player.prepare();
+			}
 			player.start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Í£Ö¹
+	 * 
 	 * @param path
 	 */
 	public static void pause() {
 		try {
-			if(player == null) player.pause();
+			if (player != null && player.isPlaying()) {
+				player.pause();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Í£Ö¹
+	 * 
 	 * @param path
 	 */
 	public static void stop() {
 		try {
-			if(player == null) player.stop();
+			if (player != null) {
+				player.stop();
+				player.reset();
+				player.release();
+				player = null;
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
